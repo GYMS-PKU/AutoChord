f86bf962bda8cac642bac06dea5840712c60962a
GYMS-PKU <18316887409@163.com>
2021-11-22 21:30:17 +0800

    new rules

--- model/ChordGenerator.py
+++ model/ChordGenerator.py
@@ -33,14 +33,19 @@ class ChordGenerator:
         if method == 'back':
             tt = len(melody)
             t = 0
-            chords = np.zeros(tt)  # 和弦
-            key_frame = []  # 关键帧，用于记录冲突
+            chords = np.zeros(tt)  # 生成的和弦序列，注意这里使用的是和弦编号，最后可以用chord_dic转为tuple
+            chords[:] = np.nan  # 先填充缺失值
+            # key_frame = []  # 关键帧，用于记录冲突
             # key_frame_feasible_chords = []  # 记录关键帧的可行集
             feasible_chords_record = []  # 记录可行集，如果回溯，则需要剔除
             while t < tt:
                 if len(feasible_chords_record) == t + 1:  # 此时说明是回溯回来的，此时不需要重复计算可行和弦
-                    feasible_chords = feasible_chords_record[-1].copy()  # 直接从这里生成下一个和弦
+                    tmp_chord = np.random.choice(feasible_chords_record[-1])[0]  # 直接从这里随机生成下一个和弦
+                    chords[t] = tmp_chord
+                    t += 1
+                    continue
 
+                # 否则这是第一次抵达该音
                 key = melody[t]  # 当前旋律音
                 feasible_chords = []  # 生成可行集，注意可行集用Rule中定义的方法判别
 
@@ -51,17 +56,28 @@ class ChordGenerator:
                     if t == 0:  # 初始第一个和弦不用检查
                         feasible_chords.append(i)
                     else:
-                        if self.rule.check_rules(chords[t-1], tmp_chord):  # 自定义的规则都检查通过后才能进入可行集
+                        if self.rule.check_rules(self.chord_dic[chords[t-1]], tmp_chord):
+                            # 自定义的规则都检查通过后才能进入可行集
                             feasible_chords.append(i)
 
-                if not feasible_chords:  # 如果没有可行的和弦，就依次往前回溯。注意此时feasible_chords_record的长度
+                if not feasible_chords:  # 如果没有可行的和弦，就依次往前回溯。注意此时feasible_chords_record的长度仍然是t
                     t -= 1
                     while len(feasible_chords_record[t]) == 1:  # 表明上一步只有一个可行集，则需要进一步回溯
                         feasible_chords_record.pop()  # 弹出最后一个
+                        chords[t] = np.nan  # 将chords该和弦取消
                         if t == 0:  # 最后一个也弹出了
                             print('No chords can satisfy rules! Please check your melody or the rules set.')
                         t -= 1
-
-                t += 1
-
-
+                    # 现在t的位置是引发矛盾的和弦
+                    feasible_chords_record[t].remove(chords[t])  # 可行集删除引发矛盾的和弦
+                    tmp_chord = np.random.choice(feasible_chords_record[-1])[0]  # 直接从这里随机生成下一个和弦
+                    chords[t] = tmp_chord
+                    t += 1
+                    continue
+                else:
+                    feasible_chords_record.append(feasible_chords)  # 可行集记录添加
+                    tmp_chord = np.random.choice(feasible_chords_record[-1])[0]  # 直接从这里随机生成下一个和弦
+                    chords[t] = tmp_chord
+                    t += 1
+                    continue
+            return chords
--- rule/Rule.py
+++ rule/Rule.py
@@ -2,6 +2,11 @@
 
 """
 该代码定义判断和弦进行是否符合规定的规则类
+在C大调中
+
+开发日志：
+2021-11-22
+-- 初始化，定义不良进行的检查
 """
 
 
@@ -30,11 +35,96 @@ class Rule:
 
     def get_rules(self):  # 定义规则
         def cross(chord_1, chord_2):  # 检查声部交叉
+            if (len(chord_1) == 1) or (len(chord_2) == 1):  # 有一个是单低音
+                return True
+            return True
+        self.rules['cross'] = cross
+
+        def parallel(chord_1, key_1, chord_2, key_2):  # 检查是否有平行八五度以及隐伏八五度
+            # 检查平行八度和隐伏八度
+            eight_2 = False  # 再检查chord_2中是否有八度
+            pos = None
+            low_2 = None
+            high_2 = None
+            for i in range(len(chord_2)):
+                if chord_2[i] == key_2:
+                    eight_2 = True
+                    pos = (i, len(chord_2) + 1)  # 记录下八度声部
+                    low_2 = chord_2[i]  # 记录声部
+                    high_2 = key_2  # 记录声部
+                    break
+            if not eight_2:
+                for i in range(len(chord_2) - 1):
+                    for j in range(i + 1, chord_2):
+                        if chord_2[i] == chord_2[j]:
+                            eight_2 = True
+                            pos = (i, j)  # 记录下声部
+                            low_2 = chord_2[i]  # 记录声部
+                            high_2 = chord_2[j]  # 记录声部
+                            break
+            if eight_2:
+                # 取出第一个和弦对应的声部
+                if pos[1] < len(chord_1) + 1:
+                    low_1 = chord_1[pos[0]]
+                    high_1 = chord_1[pos[1]] if pos[1] < len(chord_1) else key_1
+                    if low_1 == high_1:  # 平行八度
+                        return False
+                    if (abs(low_1-low_2) in [1, 2, 10, 11]) or (abs(high_1-high_2) in [1, 2, 10, 11]):  # 隐伏八度
+                        return False
+
+            # 检查平行五度和隐伏五度
+            five_2 = False  # 再检查chord_2中是否有五度
+            pos = None
+            low_2 = None
+            high_2 = None
+            for i in range(len(chord_2)):
+                if (key_2 - chord_2[i]) % 12 == 7:
+                    five_2 = True
+                    pos = (i, len(chord_2) + 1)  # 记录下八度声部
+                    low_2 = chord_2[i]  # 记录声部
+                    high_2 = key_2  # 记录声部
+                    break
+            if not five_2:
+                for i in range(len(chord_2) - 1):
+                    for j in range(i + 1, chord_2):
+                        if (chord_2[j] - chord_2[i]) % 12 == 7:
+                            five_2 = True
+                            pos = (i, j)  # 记录下声部
+                            low_2 = chord_2[i]  # 记录声部
+                            high_2 = chord_2[j]  # 记录声部
+                            break
+            if five_2:
+                # 取出第一个和弦对应的声部
+                if pos[1] < len(chord_1) + 1:
+                    low_1 = chord_1[pos[0]]
+                    high_1 = chord_1[pos[1]] if pos[1] < len(chord_1) else key_1
+                    if (high_1 - low_1) % 12 == 7:  # 平行五度
+                        return False
+                    if (abs(low_1 - low_2) in [1, 2, 10, 11]) or (abs(high_1 - high_2) in [1, 2, 10, 11]):  # 隐伏五度
+                        return False
             return True
 
-        self.rules['cross'] = cross
+        self.rules['parallel'] = parallel
 
         def repeat_three(chord_1, chord_2):  # 检查三音是否重复
             return True
 
         self.rules['repeat_three'] = repeat_three
+
+        def diagonal(chord_1, key_1, chord_2, key_2):  # 检查声部对斜
+            for i in range(len(chord_1)):
+                if i > len(chord_2):  # 一般而言现在要求和声长度一致
+                    break
+                for j in range(chord_2):
+                    if i == j:
+                        continue
+                    if abs(chord_1[i] - chord_2[j]) in [1, 11]:
+                        return False
+                if abs(chord_1[i] - key_2) in [1, 11]:
+                    return False
+            for j in range(chord_2):
+                if abs(key_1 - chord_2[j]) in [1, 11]:
+                    return False
+            return False
+
+        self.rules['diagonal'] = diagonal
